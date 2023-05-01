d31c81444b533f4f5dcf4bdd8c6f07f0f8c3b5ab
Bünyamin Korkut <90451510+bunyaminkorkut@users.noreply.github.com>
2023-05-02 02:31:59 +0300

    update for build

--- app/api/firebase/firebase.js
+++ app/api/firebase/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPgCEv-AixCyelm_QsaPmNwbTVHhSXqzw",
@@ -12,4 +11,3 @@ const firebaseConfig = {
 };
 
 export const firebaseapp = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(firebaseapp);
